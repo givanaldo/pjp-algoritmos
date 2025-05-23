935e3ec3770893d6f56aaf9bf34e5bb599bb6d9a
Givanaldo <givanaldo@gmail.com>
2025-05-23 10:18:24 -0300

    exercicios

--- while.js
+++ while.js
@@ -1,5 +1,15 @@
 let senha
-while (senha != "123456") {
+let tentativas = 0
+while (senha != "1234") {
     senha = prompt("Senha: ")
+    if (senha == "1234") {
+        alert("Acesso liberado!")
+    } else {
+        tentativas++
+        alert(`Senha incorreta! Você tem ${3-tentativas} tentativas`)
+    }
+    if (tentativas === 3) {
+        alert("Três tentativas. Conta bloqueada!")
+        break
+    }
 }
-alert("Acesso liberado!")
