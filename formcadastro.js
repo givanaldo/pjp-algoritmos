9d9cc71cc1a2304b5ecfaf04ef073a94ade9cf01
Givanaldo <givanaldo@gmail.com>
2025-05-27 22:32:52 -0300

    forms

--- formcadastro.js
+++ formcadastro.js
@@ -3,13 +3,16 @@ function cadastrar() {
   const nascimento = document.getElementById('nascimento').value;
   const uf = document.getElementById('uf').value;
 
+  const [year, month, day] = nascimento.split('-');
+  const nascimentoBr = `${day}/${month}/${year}`;
+
   const checkboxes = document.querySelectorAll('input[name="preferencias"]:checked');
   const preferencias = Array.from(checkboxes).map(cb => cb.value);
 
   const html = `
       <h3>Dados Recebidos</h3>
       <p><strong>Nome:</strong> ${nome}</p>
-      <p><strong>Data de Nascimento:</strong> ${nascimento}</p>
+      <p><strong>Data de Nascimento:</strong> ${nascimentoBr}</p>
       <p><strong>UF:</strong> ${uf}</p>
       <p><strong>Preferências:</strong> ${preferencias.length ? preferencias.join(', ') : 'Nenhuma selecionada'}</p>
     `;
