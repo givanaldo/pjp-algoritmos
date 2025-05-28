d23d584e8af5d80596ef4f8e75b1b09b228dd4ea
Givanaldo <givanaldo@gmail.com>
2025-05-28 11:15:37 -0300

    forms

--- form1.js
+++ form1.js
@@ -2,5 +2,7 @@ function exibir() {
 	let nome = document.getElementById("nome").value
 	let email = document.getElementById("email").value
 	let div = document.getElementById("resultado")
-	div.innerHTML = `Nome: ${nome} || E-mail: ${email}`
+	div.innerHTML = div.innerHTML + `Nome: ${nome} || E-mail: ${email}<br>`
+	document.getElementById("nome").value = ""
+	document.getElementById("email").value = ""
 }
