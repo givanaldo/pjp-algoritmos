49af671519213022202f7a08a3e7549e160ae253
Givanaldo <givanaldo@gmail.com>
2025-05-29 11:27:41 -0300

    forms

--- form2.js
+++ form2.js
@@ -1,8 +1,19 @@
 function exibir() {
-	let nome = document.getElementById("nome").value
-	let email = document.getElementById("email").value
-	let div = document.getElementById("resultado")
-	div.innerHTML = div.innerHTML + `Nome: ${nome} || E-mail: ${email}<br>`
-	document.getElementById("nome").value = ""
-	document.getElementById("email").value = ""
+	const confirma = confirm("Inserir a pessoa?")
+	if (confirma) {
+		let nome = document.getElementById("nome").value
+		let email = document.getElementById("email").value
+		let uf = document.getElementById("uf").value
+		let div = document.getElementById("resultado")
+		if (nome != "" && email != "" && uf != "") {
+			div.innerHTML = div.innerHTML +
+				`<li><b>Nome:</b> ${nome} (${email}) || <b>UF:</b> ${uf}</li>`
+			document.getElementById("nome").value = ""
+			document.getElementById("email").value = ""
+			document.getElementById("uf").value = ""
+		}
+	} else {
+		alert("Pessoa não foi inserida!")
+	}
+
 }
